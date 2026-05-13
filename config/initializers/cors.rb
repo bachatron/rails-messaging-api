e6fa4993eab528ab1269cef1dcc07d0d23f45b66
Desktop <idz420@gmail.com>
2026-05-13 20:21:36 -0300

    Configure API base URL for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173" # Vite's default port
+    origins "*"
 
     resource "*",
       headers: :any,
