7339c97f2dd75e025a9642123cc149bfe024f9cc
Desktop <idz420@gmail.com>
2026-05-13 22:36:02 -0300

    Make Cloudinary initializer safe for CI

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,8 @@
-Cloudinary.config do |config|
-  config.cloud_name = Rails.application.credentials.cloudinary[:cloud_name]
-  config.api_key    = Rails.application.credentials.cloudinary[:api_key]
-  config.api_secret = Rails.application.credentials.cloudinary[:api_secret]
-  config.secure     = true
+if ENV["CLOUDINARY_CLOUD_NAME"].present?
+  Cloudinary.config do |config|
+    config.cloud_name = ENV["CLOUDINARY_CLOUD_NAME"]
+    config.api_key = ENV["CLOUDINARY_API_KEY"]
+    config.api_secret = ENV["CLOUDINARY_API_SECRET"]
+    config.secure = true
+  end
 end
